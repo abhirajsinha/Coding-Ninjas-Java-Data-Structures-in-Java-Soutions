29fa29bcb473df865d3bac1827068bb8a820c63e
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-10-31 07:01:20 +0530

    Create Valid statements.java

---
+++ Lecture 2 : OOPS 2/Valid statements.java
@@ -0,0 +1,8 @@
+Select the valid statement(s) on the basis of following code.
+Both classes are in same package.
+class Vehicle {}
+class Car extends Vehicle {}
+
+Answer
+b
+Class Car is inheriting Properties of Class vehicle

f979619c45593b69af54fda39ada851f59664c83
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-11-01 23:16:26 +0530

    Create Correct option(3).java

---
+++ Lecture 2 : OOPS 2/Correct option(3).java
@@ -0,0 +1,44 @@
+The following class Vehicle, declares a protected variable name, inside package p1:
+package p1;
+
+public class Vehicle {
+    protected String name;
+}
+Which of the following code/codes will give error
+A. 
+package p1;
+
+public class Car {
+    void props() {
+        Vehicle v = new Vehicle();
+        v.name = "Vehicle 1st";      
+    }
+}
+
+B. 
+package p2;
+
+import p1.Vehicle;
+
+class Car extends Vehicle {
+    public static void main(String args[]) {
+        Car c = new Car();
+        c.name = "Ferrari";
+    }
+}
+
+C. 
+package p2;
+
+import p1.Vehicle;
+
+class Car {
+    void props() {
+        Vehicle v = new Vehicle();
+        v.name = "BMW";
+    }
+}
+
+
+Answer
+C

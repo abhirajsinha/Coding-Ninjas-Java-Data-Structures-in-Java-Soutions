02405efdf79f06e54f9bf8f21ce76aa8d42da714
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-11-12 07:36:52 +0530

    Create What is the output(11).java

---
+++ Lecture 2 : OOPS 2/What is the output(11).java
@@ -0,0 +1,26 @@
+Select the output of the following program
+class A{
+    public static void A(){
+        System.out.print("Good ");
+        throw new RuntimeException();
+    }
+    public static void B(){
+         A();
+         System.out.print("This ");
+    }
+    public static void main(String[] args){
+        try{
+           B();
+        }
+        catch(Exception e){
+           System.out.print("Time ");
+        }
+        finally{
+           System.out.print("at CodingNinjas");
+        }
+    }
+}
+
+
+Answer:
+Good Time at CodingNinjas

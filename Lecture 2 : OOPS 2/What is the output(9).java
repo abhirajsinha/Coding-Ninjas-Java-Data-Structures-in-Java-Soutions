7d00b2dc3020b86852c8ecca4bb40ec4d142104f
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-11-12 07:35:06 +0530

    Create What is the output(9).java

---
+++ Lecture 2 : OOPS 2/What is the output(9).java
@@ -0,0 +1,16 @@
+Select the output of the following program
+class A{
+    public static void main(String[] args){
+
+    try{
+        return;
+    }
+    finally{
+        System.out.println("Hello coding ninjas");
+    }
+  }
+}
+
+
+Answer
+Hello coding ninjas

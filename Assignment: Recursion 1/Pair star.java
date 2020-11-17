9583deeb7b1904a6008f1bedab5ec255c65ad49c
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-11-17 22:39:15 +0530

    Create Pair star.java

---
+++ Assignment: Recursion 1/Pair star.java
@@ -0,0 +1,41 @@
+/*
+Pair star
+Send Feedback
+Given a string S, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+Input format :
+String S
+Output format :
+Modified string
+Constraints :
+0 <= |S| <= 1000
+where |S| represents length of string S.
+Sample Input 1 :
+hello
+Sample Output 1:
+hel*lo
+Sample Input 2 :
+aaaa
+Sample Output 2 :
+a*a*a*a
+*/
+
+
+public class solution {
+
+    public static String addStars(String s) {
+        
+	    if(s.length()==1){
+            return s;
+        }
+        
+        String smallAns=addStars(s.substring(1));
+        
+        if(s.charAt(0)==smallAns.charAt(0)) {
+        	
+        	return s.charAt(0)+"*"+smallAns;
+        }
+        
+        else
+        	return s.charAt(0)+smallAns;
+	}
+}
